fae48a823c791dad4153125879986e0f1d5aa628
j_kolotovkina <kolotovkinajulia@gmail.com>
2017-09-08 17:12:22 +0300

    customize welcome page

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,18 +1,52 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LoginPage } from "../auth/login/login";
+import { RegisterPage } from "../auth/register/register";
 
 @Component({
   selector: 'page-welcome',
   templateUrl: 'welcome.html',
 })
 export class WelcomePage {
+  public sliders: Array<any>;
 
   constructor(public navCtrl: NavController) {
   }
 
-  goToLogin(){
+  ionViewCanEnter(){
+    this._initSlider();
+  }
+
+  goToLogin() {
     this.navCtrl.setRoot(LoginPage);
+  }
+  goToRegister() {
+    this.navCtrl.setRoot(RegisterPage);
+  }
+
+  private _initSlider() {
+    return this.sliders = [
+      {
+        text: `LineUp is a collaboration
+                platform for In uencers, Brand Enthusiasts`,
+        icon: 'star'
+      }, {
+        text: `Get Campaign invitations based 
+                on your marketing variables and prole strength.`,
+        icon: 'user'
+      }, {
+        text: `Collaborate with brands & agencies. Take part in
+                market research & advertising campaigns`,
+        icon: 'bell'
+      }, {
+        text: `Get Real-time data on your
+                personal growth.`,
+        icon: 'pie-chart'
+      }, {
+        text: `Get rewarded in cash,
+                products and exclusive event invitations`,
+        icon: 'star'
+      }]
   }
 
 }
